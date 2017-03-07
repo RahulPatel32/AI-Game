688f4552a84c093e278afd210f18c916e8e635f9
Rahul <patelr32@coventry.ac.uk>
2017-03-07 12:10:38 +0000

    AI moving

---
+++ Assets/Scripts/GuardController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardController : MonoBehaviour {
+
+    public AIwaypoint[] path;
+    public float reachDist;
+    private int currentWaypoint;
+    public bool pause;
+    public float speed;
+
+	void Start() {
+		reachDist = 0;
+		currentWaypoint = 0;
+	}
+
+    void Update()
+    {
+
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        float distance = Vector3.Distance(path[currentWaypoint].transform.position, transform.position);
+
+        if(!pause)
+        {
+            transform.Rotate(0, 0, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, path[currentWaypoint].transform.position, speed * Time.deltaTime);
+            if(distance == reachDist)
+            {
+                StartCoroutine(GuardPause());
+                currentWaypoint++;
+            }
+            if(currentWaypoint ==path.Length)
+            {
+                currentWaypoint = 0;
+            }
+        }
+    }
+
+    void DetectPlayer()
+    {
+
+    }
+
+    IEnumerator GuardPause()
+    {
+        pause = true;
+        yield return new WaitForSeconds(path[currentWaypoint].pauseTime);
+        pause = false;
+    }
+
+}
---
+++ Assets/Scripts/Not Used/AIpatrol.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIpatrol : MonoBehaviour {
+
+    public Transform[] Waypoints;
+    public float Speed;
+    public int curWayPoint;
+    public bool doPatrol = true;
+    public Vector3 Target;
+    public Vector3 MoveDirection;
+    public Vector3 Velocity;
+    private Rigidbody2D rigidBody2d;
+
+    void Start()
+    {
+        rigidBody2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if(curWayPoint < Waypoints.Length)
+        {
+            Target = Waypoints[curWayPoint].position;
+            MoveDirection = Target - transform.position;
+            Velocity = rigidBody2d.velocity;
+
+            if(MoveDirection.magnitude < 1)
+            {
+                curWayPoint++;
+            }
+            else
+            {
+                Velocity = MoveDirection.normalized * Speed;
+            }
+        }
+        else
+        {
+            if(doPatrol)
+            {
+                curWayPoint = 0;
+            }
+            else
+            {
+                Velocity = Vector2.zero;
+            }
+        }
+
+        rigidBody2d.velocity = Velocity;
+        transform.LookAt(Target);
+    }
+}
